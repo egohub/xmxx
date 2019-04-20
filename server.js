07bfe5dfcc3979f456b2cedd5ece39c45aad7f13
ego <ego@hackermail.com>
2019-04-21 04:03:08 +0630

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ var express = require('express'),
     request = require('sync-request')
    app = express();
    app.use(express.static(__dirname + '/public'));
-
+    
+   app.all('*', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next();
+  });
 //   api        = require('./routes')
 
 //app.get('/posts', api.posts);
